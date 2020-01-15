23cdf83c04eb35228a1eefee7f75d2b9b090a680
drmwnnnnn <48938842+drmwnnnnn@users.noreply.github.com>
2020-01-15 09:36:19 +0700

    Test Reminder

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Perizinan;
+use App\PostPerizinan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
     public function index(){
-        return view('dashboard/index');
+        date_default_timezone_set('Asia/Bangkok');
+
+        // $dateTemp = Perizinan::find($id);
+        
+
+        $now = now();
+        $dates =  "2020-01-13 10:00:00.000000 Asia/Bangkok (+07:00)";
+        $dates = substr_replace($now,"",-9);
+        dd($now,$dates);
+
+        // return view('dashboard/index');
+
     }
 }
