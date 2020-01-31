84fc46584e286c94369277b559de2c0f5e4629cf
MAgungHKM <maranga12341@yahoo.com>
2020-01-31 16:05:35 +0700

    Clean the Code

--- resources/views/accessuser/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts/master')
-
-@section('content')
-@if(auth()->user()->role == 'access_user')
-    <h1>Dashboard Full Access User</h1>
-@endif
-@stop
--- resources/views/stduser/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts/master')
-
-@section('content')
-@if(auth()->user()->role == 'std_user')
-    <h1>Dashboard Standard User</h1>
-@endif  
-@stop
